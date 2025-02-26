fcff3f7781ac7f06b2166d6a02830a22f8f927eb
bhavya534 <padaliyabhavya5@gmail.com>
2025-02-26 23:00:55 +0530

    Small Change Added

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("register.html")
 @app.route("/register")
 def register():
     return render_template("register.html")
